4b9971457e3ad44b1a891dc2daec3018eb991779
Mario Antonio Polo Daza <marioapd@ext.inditex.com>
2020-04-27 22:23:53 +0200

    clean master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
-
-import { StudentListModule } from './modules/student-list/student-list.module';
-import { StudentDetailModule } from './modules/student-detail/student-detail.module';
 
 @NgModule({
   declarations: [
@@ -16,10 +12,6 @@ import { StudentDetailModule } from './modules/student-detail/student-detail.mod
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // App Modules
-    SharedModule,
-    StudentListModule,
-    StudentDetailModule
   ],
   providers: [],
   bootstrap: [AppComponent]
